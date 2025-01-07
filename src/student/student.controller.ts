527b15c00ccefab1089db7c8f97b58052bde82c2
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 14:06:28 +0800

    updates

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, Post, Put,Delete,Body,Param, } from '@nestjs/common';
+  import { StudentService } from './student.service';
+  import { Student } from './entities/student.entity';
+  
+  @Controller('students')
+  export class StudentController {
+    constructor(private readonly studentService: StudentService) {}
+  
 
-@Controller('student')
-export class StudentController {}
+     @Get()
+    async findAll(): Promise<Student[]> {
+      return await this.studentService.findAll();
+   }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<void> {
+      return await this.studentService.delete(id);
+}
+    @Put(':id')
+    async update(
+      @Param('id') id: number,
+      @Body() student: Partial<Student>,
+    ): Promise<Student> {
+      return await this.studentService.update(id, student);
+} 
+
+    }
+  
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    update(id: number, student: Partial<import("./entities/student.entity").Student>): import("./entities/student.entity").Student | PromiseLike<import("./entities/student.entity").Student> {
+        throw new Error('Method not implemented.');
+    }
+    findAll(): import("./entities/student.entity").Student[] | PromiseLike<import("./entities/student.entity").Student[]> {
+        throw new Error('Method not implemented.');
+    }
+    delete(id: number): void | PromiseLike<void> {
+        throw new Error('Method not implemented.');
+    }
+}

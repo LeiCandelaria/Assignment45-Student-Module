d1dde2f825cc3c6b8c480e62907da0ef933d3f4e
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 14:56:33 +0800

    updated service and controller

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {Controller, Get, Post,Put,Delete,Body,Param,} from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './entities/student.entity';
 
@@ -6,8 +6,24 @@ import { Student } from './entities/student.entity';
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  @Post()
-  create(@Body() student: Partial<Student>) {
-    return this.studentService.create(student);
-  }
+
+   @Get()
+  async findAll(): Promise<Student[]> {
+    return await this.studentService.findAll();
+ }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return await this.studentService.delete(id);
 }
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() student: Partial<Student>,
+  ): Promise<Student> {
+    return await this.studentService.update(id, student);
+} 
+
+  }
+
+

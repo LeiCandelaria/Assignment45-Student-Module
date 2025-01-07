2c5dd888e974f8a6d7b77d68f79a2f670ca61925
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 13:40:10 +0800

    created create student function

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Post()
+  create(@Body() student: Partial<Student>) {
+    return this.studentService.create(student);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+  async create(student: Partial<Student>): Promise<Student> {
+    return this.studentRepository.save(student);
+  }
+}

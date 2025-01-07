caae755fca2d3441f24b5e374d60beaa3f6bba12
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 18:42:25 +0800

    updated service on main

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,29 @@
+
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './entities/student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  create(student: Partial<Student>) {
+    throw new Error('Method not working.');
+  }
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+
+async findAll(): Promise<Student[]> {
+    return await this.studentRepository.find();
+}
+
+  async delete(id: number): Promise<void> {
+    await this.studentRepository.delete(id);
+}
+  async update(id: number, student: Partial<Student>): Promise<Student> {
+    await this.studentRepository.update(id, student);
+    return await this.studentRepository.findOne({ where: { id } });
+}
+
+  }

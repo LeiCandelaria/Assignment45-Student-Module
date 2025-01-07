26adb8dfd8dcabb4ff4d2da481f4ce6ba5c4bf2c
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 13:21:53 +0800

    new commit

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
 import { StudentController } from './student.controller';
 
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Student])],
   providers: [StudentService],
-  controllers: [StudentController]
+  controllers: [StudentController],
 })
 export class StudentModule {}

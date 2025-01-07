ea45cbda98565ce813535d81de6edd6a1c23e822
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-07 14:25:35 +0800

    created delete function

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body,Get,Patch,Param,Delete} from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Post()
+  create(@Body() student: Partial<Student>) {
+    return this.studentService.create(student);
+  }
+  @Get()
+findAll() {
+  return this.studentService.findAll();
+}
+@Patch(':id')
+update(@Param('id') id: number, @Body() updateData: Partial<Student>) {
+  return this.studentService.update(id, updateData);
+}
+@Delete(':id')
+remove(@Param('id') id: number) {
+  return this.studentService.remove(id);
+}
+
+} 

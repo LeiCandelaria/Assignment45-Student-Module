bdc6f3615fa2b49a1401c3454e1120d31b84dd54
LeiCandelaria <leilaanne.candelaria@cvsu.edu.ph>
2025-01-08 17:30:42 +0800

    updated db

--- src/student_db.sql
+++ src/student_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2025 at 03:37 PM
+-- Generation Time: Jan 08, 2025 at 10:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -12,19 +12,25 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 --
--- Database: `student_db`
----
-----------------------------------------------------------------
-
----table student attributes--
+-- Table structure for table `student`
+--
 
 CREATE TABLE `student` (
-  `id` int(13) NOT NULL,
+  `id` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `enrollmentdate` date NOT NULL DEFAULT current_timestamp(),
-  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
-  `updatedAt` datetime(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `student` (`id`, `firstName`, `lastName`, `email`, `createdAt`, `updatedAt`) VALUES
+(131315, 'Wendnesday', 'Addams', 'fridayaddams@nevermorenightshades.com', '2025-01-08 09:16:44', '2025-01-08 09:16:44');
+
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IDX_a56c051c91dbe1068ad683f536` (`email`);
+
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=131316;
 COMMIT;
